8bf560485e7c08268ddd32324ff6dd774b3be8a2
Rishikesh Agrawani <rishikesh@moneybloom.in>
2019-06-26 11:59:53 +0530

    static files setting, templates, files download

--- src/nsessoracle/settings.py
+++ src/nsessoracle/settings.py
@@ -140,3 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'root')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static', 'files'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
--- src/nsessoracle/urls.py
+++ src/nsessoracle/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from users.views import UserLogin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', UserLogin.as_view(), name='users-login'),
@@ -24,3 +26,6 @@ urlpatterns = [
     re_path(r'^tests/users/', include('users.urls', namespace='users'))
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
